71e087ff7a3a0ee0c046822b3da9e19a3820858b
Roberto <45430436+gitrobdev@users.noreply.github.com>
2022-12-17 23:47:42 +0100

    bug-fix on the S2PJ algorithm and new plots

--- python/mylib.py
+++ python/mylib.py
@@ -3,6 +3,7 @@ import scipy.sparse as sparse
 import scipy.linalg as linalg
 from scipy import stats
 
+
 def format_func(value, tick_number):
     # find number of multiples of pi/2
     N = int(np.round(2 * value / np.pi))
@@ -17,7 +18,9 @@ def format_func(value, tick_number):
     else:
         return r"${0}\pi$".format(N // 2)
 
+
 ##################################################################################################
+
 
 def fvm_laplacian_1D(nu, leftBC, rightBC, N, L, kwargs):
     """generate the sparse matrix of the second order finite difference scheme"""
@@ -26,7 +29,6 @@ def fvm_laplacian_1D(nu, leftBC, rightBC, N, L, kwargs):
     diag = np.zeros(N)
     lower = np.zeros(N-1)
     upper = np.zeros(N-1)
-    
     for k in range(N): #k=0,1,...,N-1
         if k==0: #first cell
             if leftBC['type']=='dirichlet':
@@ -50,8 +52,8 @@ def fvm_laplacian_1D(nu, leftBC, rightBC, N, L, kwargs):
             diag[k] = -(nu(k*dx, kwargs) + nu((k+1)*dx, kwargs))
             lower[k-1] = nu(k*dx, kwargs)
             upper[k] = nu((k+1)*dx, kwargs)
-
     return sparse.csc_matrix(sparse.diags([diag/np.square(dx), lower/np.square(dx), upper/np.square(dx)], [0, -1, 1])), fBC
+
 
 def fvm_reconstruct_1D(sol):
     """second order accuracy reconstructor for cell faces"""
@@ -66,16 +68,20 @@ def fvm_reconstruct_1D(sol):
             rec[i] = 0.5*(sol[i-1]+sol[i])
     return rec
 
+
 def slope(residual):
     """compute the slope of the error function in log scale"""
     x = np.arange(len(residual))
     slope, intercept, r_value, p_value, std_err = stats.linregress(x,np.log10(residual))
     return slope
 
+
 def algebraic_residual(A, B, C, D, u, v, f1, f2):
     return np.sqrt(np.square(np.linalg.norm(f1-A@u-B@v)) + np.square(np.linalg.norm(f2-C@u-D@v)))
 
+
 ##################################################################################################
+
 
 def method_BlockJacobi(A, B, C, D, f1, f2, nit, toll):
     u  = np.zeros(A.shape[0])
@@ -94,6 +100,7 @@ def method_BlockJacobi(A, B, C, D, f1, f2, nit, toll):
         if res[-1] < toll: break
     return u, v, np.array(res), nitfinal
 
+
 def method_BlockGaussSeidel(A, B, C, D, f1, f2, nit, toll):
     nitfinal = 0
     u = np.zeros(A.shape[0])
@@ -106,6 +113,7 @@ def method_BlockGaussSeidel(A, B, C, D, f1, f2, nit, toll):
         res.append(algebraic_residual(A, B, C, D, u, v, f1, f2))
         if res[-1] < toll: break
     return u, v, np.array(res), nitfinal
+
 
 # def method_BlockSOR(A, B, C, D, f1, f2, nit, alpha, toll):
 #     nitfinal = 0
@@ -123,32 +131,6 @@ def method_BlockGaussSeidel(A, B, C, D, f1, f2, nit, toll):
 #         if res[-1] < toll: break
 #     return u, v, np.array(res), nitfinal
 
-def method_Lscheme(A, B, C, D, f1, f2, nit, toll, type, L):
-    nitfinal = 0
-    u = np.zeros(A.shape[0])
-    v = np.zeros(A.shape[0])
-    res = []
-    for i in range(nit):
-        nitfinal += 1
-
-        # L on both
-        if type=='both':
-            u = sparse.linalg.spsolve((L*sparse.eye(A.shape[0])) + A, f1 + L*u - B@v)
-            v = sparse.linalg.spsolve((L*sparse.eye(A.shape[0])) + D, f2 + L*v - C@u)
-        
-        # L on u
-        if type=='L_on_u':
-            u = sparse.linalg.spsolve((L*sparse.eye(A.shape[0])) + A, f1 - B@v + L*u)
-            v = sparse.linalg.spsolve(D, f2 - C@u)
-
-        # L on v
-        if type=='L_on_v':
-            v = sparse.linalg.spsolve((L*sparse.eye(A.shape[0])) + D, f2 - C@u + L*v)
-            u = sparse.linalg.spsolve(A, f1 - B@v)
-
-        res.append(algebraic_residual(A, B, C, D, u, v, f1, f2))
-        if res[-1] < toll: break
-    return u, v, np.array(res), nitfinal
 
 def method_ShurPartialJacobi(A, B, C, D, f1, f2, nit, toll, type):
     nitfinal = 0
@@ -161,27 +143,21 @@ def method_ShurPartialJacobi(A, B, C, D, f1, f2, nit, toll, type):
     res = []
     for i in range(nit):
         nitfinal += 1
-
-        # ALTERNATE
         if type=='alternate':
-            u = sparse.linalg.spsolve(A - B@invDD@C, f1 - B@invDD@(f2-(D-DD)@v))
-            v = sparse.linalg.spsolve(D - C@invAA@B, f2 - C@invAA@(f1-(A-AA)@u))
-        
-        # APPROXIMATED SHUR ON u
+            u = sparse.linalg.spsolve(A - B@invDD@C, f1 - B@invDD@(f2 + (DD-D)@v))
+            v = sparse.linalg.spsolve(D - C@invAA@B, f2 - C@invAA@(f1 + (AA-A)@u))
         if type=='shur_on_u':
-            u = sparse.linalg.spsolve(A - B@invDD@C, f1 - B@invDD@(f2-(D-DD)@v))
-            v = sparse.linalg.spsolve(D, f2 - C@u)
-
-        # APPROXIMATED SHUR ON v
+            u = sparse.linalg.spsolve(A - B@invDD@C, f1 - B@invDD@(f2 + (DD-D)@v))
+            v = sparse.linalg.spsolve(            D, f2 - C@u)
         if type=='shur_on_v':
-            v = sparse.linalg.spsolve(D - C@invAA@B, f2 - C@invAA@(f1-(A-AA)@u))
-            u = sparse.linalg.spsolve(A, f1 - B@v)
-
+            v = sparse.linalg.spsolve(D - C@invAA@B, f2 - C@invAA@(f1 + (AA-A)@u))
+            u = sparse.linalg.spsolve(            A, f1 - B@v)
         res.append(algebraic_residual(A, B, C, D, u, v, f1, f2))
         if res[-1] < toll: break
     return u, v, np.array(res), nitfinal
 
-def method_SPJ_stab(A, B, C, D, f1, f2, nit, toll, type):
+
+def method_SPJ_stab(A, B, C, D, f1, f2, nit, toll, type='alternate'):
     nitfinal = 0
     AA    = sparse.csc_matrix(np.diag(A.diagonal(), 0))
     invAA = sparse.csc_matrix(np.diag(1/A.diagonal(), 0))
@@ -192,27 +168,21 @@ def method_SPJ_stab(A, B, C, D, f1, f2, nit, toll, type):
     res = []
     for i in range(nit):
         nitfinal += 1
-
-        # ALTERNATE
         if type=='alternate':
             u = sparse.linalg.spsolve(A - B@invDD@C, f1 - B@v - B@invDD@C@u)
             v = sparse.linalg.spsolve(D - C@invAA@B, f2 - C@u - C@invAA@B@v)
-        
-        # APPROXIMATED SHUR ON u
         if type=='shur_on_u':
             u = sparse.linalg.spsolve(A - B@invDD@C, f1 - B@v - B@invDD@C@u)
-            v = sparse.linalg.spsolve(D, f2 - C@u)
-
-        # APPROXIMATED SHUR ON v
+            v = sparse.linalg.spsolve(            D, f2 - C@u)
         if type=='shur_on_v':
             v = sparse.linalg.spsolve(D - C@invAA@B, f2 - C@u - C@invAA@B@v)
-            u = sparse.linalg.spsolve(A, f1 - B@v)
-
+            u = sparse.linalg.spsolve(            A, f1 - B@v)
         res.append(algebraic_residual(A, B, C, D, u, v, f1, f2))
         if res[-1] < toll: break
     return u, v, np.array(res), nitfinal
 
-def method_ShurDualPartialJacobi(A, B, C, D, f1, f2, nit, toll):
+
+def method_ShurDualPartialJacobi(A, B, C, D, f1, f2, nit, toll, type='alternate'):
     nitfinal = 0
     AA    = sparse.csc_matrix(np.diag(A.diagonal(), 0))
     invAA = sparse.csc_matrix(np.diag(1/A.diagonal(), 0))
@@ -225,19 +195,38 @@ def method_ShurDualPartialJacobi(A, B, C, D, f1, f2, nit, toll):
     res = []
     for i in range(nit):
         nitfinal += 1
+        if type=='alternate':
+            # u = sparse.linalg.spsolve(A - BB@invDD@C, f1 - BB@invDD@f2 - ((B-BB)@invDD@D)@v)  ??????
+            # v = sparse.linalg.spsolve(D - CC@invAA@B, f2 - CC@invAA@f1 - ((C-CC)@invAA@A)@u)  ??????
+            u = sparse.linalg.spsolve(A - BB@invDD@C, f1 - BB@invDD@(f2 - D@v) - B@v)
+            v = sparse.linalg.spsolve(D - CC@invAA@B, f2 - CC@invAA@(f1 - A@u) - C@u)
+        if type=='shur_on_u':
+            u = sparse.linalg.spsolve(A - BB@invDD@C, f1 - BB@invDD@(f2 - D@v) - B@v)
+            v = sparse.linalg.spsolve(D, f2 - C@u)
+        if type=='shur_on_v':
+            v = sparse.linalg.spsolve(D - CC@invAA@B, f2 - CC@invAA@(f1 - A@u) - C@u)
+            u = sparse.linalg.spsolve(A, f1 - B@v)
+        res.append(algebraic_residual(A, B, C, D, u, v, f1, f2))
+        if res[-1] < toll: break
+    return u, v, np.array(res), nitfinal
 
-        # S2PJ alternate
-        u = sparse.linalg.spsolve(A - BB@invDD@C, f1 - BB@invDD@f2 - ((B-BB)@invDD@D)@v)
-        v = sparse.linalg.spsolve(D - CC@invAA@B, f2 - CC@invAA@f1 - ((C-CC)@invAA@A)@u)
 
-        # S2PJ ON u
-        # u = sparse.linalg.spsolve(A - BB@invDD@C, f1 - BB@invDD@f2 - (B-BB@invDD@D)@v)
-        # v = sparse.linalg.spsolve(D, f2 - C@u)
-
-        # S2PJ ON v
-        # v = sparse.linalg.spsolve(D - CC@invAA@B, f2 - CC@invAA@f1 - (C-CC@invAA@A)@u)
-        # u = sparse.linalg.spsolve(A, f1 - B@v)
-
+def method_Lscheme(A, B, C, D, f1, f2, nit, toll, type, Lu, Lv):
+    nitfinal = 0
+    u = np.zeros(A.shape[0])
+    v = np.zeros(A.shape[0])
+    res = []
+    for i in range(nit):
+        nitfinal += 1        
+        if type=='both':
+            u = sparse.linalg.spsolve((Lu*sparse.eye(A.shape[0])) + A, f1 + Lu*u - B@v)
+            v = sparse.linalg.spsolve((Lv*sparse.eye(A.shape[0])) + D, f2 + Lv*v - C@u)
+        if type=='L_on_u':
+            u = sparse.linalg.spsolve((Lu*sparse.eye(A.shape[0])) + A, f1 - B@v + Lu*u)
+            v = sparse.linalg.spsolve(D, f2 - C@u)
+        if type=='L_on_v':
+            v = sparse.linalg.spsolve((Lv*sparse.eye(A.shape[0])) + D, f2 - C@u + Lv*v)
+            u = sparse.linalg.spsolve(A, f1 - B@v)
         res.append(algebraic_residual(A, B, C, D, u, v, f1, f2))
         if res[-1] < toll: break
     return u, v, np.array(res), nitfinal
