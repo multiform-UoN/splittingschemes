4ae2cf9073a6e2f2ba5ebd5da0f83af401c68ff0
Matteo Icardi <mat.icardi@gmail.com>
2023-05-17 22:02:24 +0100

    improved python script to study modified uzawa

--- iterativeDarcy/iterativeDarcy.py
+++ iterativeDarcy/iterativeDarcy.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy import sparse
 
 def randpd(n):
     # Generate a random positive definite matrix of size n x n
@@ -33,13 +34,41 @@ def randtridiagpd(n):
 
     return A
 
+def power_iteration(matrix, num_iterations=100):
+    n = matrix.shape[0]
+    x = np.ones(n)
+    for _ in range(num_iterations):
+        x = matrix.dot(x)
+        x /= np.linalg.norm(x)
+    eigenvalue = x.dot(matrix.dot(x))
+    return eigenvalue
+
+def count_eigenvalues(matrix):
+    eigenvalues, _ = np.linalg.eig(matrix)
+    eigenvalues_real = np.real(eigenvalues)
+    positive_eigenvalues = np.sum(eigenvalues_real > 0)
+    negative_eigenvalues = np.sum(eigenvalues_real < 0)
+    largest_positive_eigenvalue = np.max(eigenvalues_real[eigenvalues_real > 0]) if positive_eigenvalues > 0 else None
+    largest_negative_eigenvalue = np.min(eigenvalues_real[eigenvalues_real < 0]) if negative_eigenvalues > 0 else None
+    condition_number = np.linalg.cond(matrix)
+    
+    print("Number of positive eigenvalues:", positive_eigenvalues)
+    print("Number of negative eigenvalues:", negative_eigenvalues)
+    print("Largest positive eigenvalue:", largest_positive_eigenvalue)
+    print("Largest negative eigenvalue:", largest_negative_eigenvalue)
+    print("Condition number:", condition_number)
+
 # Parameters
-n = 80
-m = 30
+n = 200
+m = 100
 max_it = 200
 tol = 1e-3
 beta = 1  # parameter that controls the coupling
 log = False
+maxalpha = 10
+minalpha = 0.1
+maxbeta = 10
+minbeta = -10
 
 # Saddle point
 # M = randpd(n)
@@ -56,26 +85,58 @@ log = False
 
 # # # Generic coupling
 M = randpd(n)
-M = M + randtridiagpd(n)
-D = -np.random.randn(m,m)
+M = randtridiagpd(n)
+# D = -np.random.randn(m,m)
 D = randtridiagpd(m)/beta
 Bt = beta*np.random.randn(n, m)
 B = beta*np.random.randn(m, n)
 B = -Bt.T
 omega = 0
 
+
 # linear system
+print("Assemble and solve block-coupled system A")
 A = np.block([[M, Bt], [B, D]])
 f = np.random.rand(n + m, 1)
 x = np.linalg.solve(A, f)
-print("Condition number:", np.linalg.cond(A))
 
+# Count eigenvalues
+count_eigenvalues(A)
 
-# Iterative method
+# Assemble Schur-based factorisations for Peters' method
+print("\n")
+print("Assemble and analyse the Schur complements")
 Mhm1 = np.linalg.inv(np.diag(np.diag(M)))
 S = np.dot(B, np.dot(Mhm1, Bt))    # approximate Schur
 Q = np.block([[M, np.zeros((n, m))], [B, -np.eye(m)]])  # Uzawa
 R = np.block([[np.eye(n), np.dot(np.linalg.inv(M), Bt)], [np.zeros((m, n)), S-D]])  # Peters paper
+
+# convergence of Peters' modified Uzawa method
+Sexact = np.dot(B, np.dot(np.linalg.inv(M), Bt))    # approximate Schur
+rho = power_iteration(np.eye(m)-np.dot(np.linalg.inv(S-D),Sexact-D))
+print("Spectral radius of Peters' method", rho)
+if abs(rho)<1:
+    print("Peters' method will converge!")
+else:
+    print("Peters' method will not converge, check methods with alpha and beta")
+
+print("\n")
+
+# Count eigenvalues
+print("Exact Schur complement")
+count_eigenvalues(Sexact)
+
+print("\n")
+
+# Count eigenvalues
+print("Approximate Schur complement")
+count_eigenvalues(S)
+
+# # scipy version
+# sA = sparse.csr_matrix(A)
+# x = sparse.linalg.spsolve(sA,f)
+
+print("\n")
 
 ########################
 ########################
@@ -87,8 +148,9 @@ z = np.linalg.solve(Q, r)
 z0 = z.copy()
 for i in range(max_it):
     alpha = np.dot(z.flatten(),r.flatten())/np.dot(z.flatten(),A.dot(z).flatten())
-    alpha = min(max(alpha,0.1),1.0)
-    # print('alpha ', alpha)
+    if log:
+        print('alpha ', alpha)
+    alpha = min(max(alpha,minalpha),maxalpha)
     xk = x0 - alpha*z
     r = A.dot(xk) - f
     # print(np.linalg.norm(r))
@@ -98,7 +160,9 @@ for i in range(max_it):
     z0 = z
     z = np.linalg.solve(Q, r)
     beta = np.dot(z.flatten(),z0.flatten())/np.dot(z0.flatten(),z0.flatten())
-    # print('beta ', beta)
+    if log:
+        print('beta ', beta)
+    beta = min(max(beta,minbeta),maxbeta)
     z = z - beta*z0
     x0 = xk
 if i==max_it-1:
@@ -135,9 +199,9 @@ z = np.linalg.solve(R, np.linalg.solve(Q, r))
 z0 = z.copy()
 for i in range(max_it):
     alpha = np.dot(z.flatten(),r.flatten())/np.dot(z.flatten(),A.dot(z).flatten())
-    alpha = min(max(alpha,0.1),1.0)
     if log:
         print('alpha ', alpha)
+    alpha = min(max(alpha,minalpha),maxalpha)
     xk = x0 - alpha*z
     r = A.dot(xk) - f
     if log:
@@ -148,7 +212,9 @@ for i in range(max_it):
     z0 = z
     z = np.linalg.solve(R, np.linalg.solve(Q, r))
     beta = np.dot(z.flatten(),z0.flatten())/np.dot(z0.flatten(),z0.flatten())
-    # print('beta ', beta)
+    if log:
+        print('beta ', beta)
+    beta = min(max(beta,minbeta),maxbeta)
     z = z - beta*z0
     x0 = xk
 if i==max_it-1:
@@ -165,9 +231,9 @@ r = A.dot(x0) - f
 for i in range(max_it):
     z = np.linalg.solve(R, np.linalg.solve(Q, r))
     alpha = np.dot(z.flatten(),r.flatten())/np.dot(z.flatten(),A.dot(z).flatten())
-    alpha = min(max(alpha,0.1),1.0)
     if log:
         print('alpha ', alpha)
+    alpha = min(max(alpha,minalpha),maxalpha)
     xk = x0 - alpha*z
     r = A.dot(xk) - f
     if log:
@@ -191,9 +257,9 @@ z = np.linalg.solve(R, np.linalg.solve(Q, r))
 z0 = z.copy()
 for i in range(max_it):
     alpha = np.dot(z.flatten(),A.dot(r).flatten())/np.dot(A.dot(z).flatten(),A.dot(z).flatten())
-    alpha = min(max(alpha,0.1),1.0)
     if log:
         print('alpha ', alpha)
+    alpha = min(max(alpha,minalpha),maxalpha)
     xk = x0 - alpha*z
     r = A.dot(xk) - f
     if log:
@@ -204,7 +270,9 @@ for i in range(max_it):
     z0 = z
     z = np.linalg.solve(R, np.linalg.solve(Q, r))
     beta = np.dot(z.flatten(),A.dot(z0).flatten())/np.dot(A.dot(z0).flatten(),A.dot(z0).flatten())
-    # print('beta ', beta)
+    if log:
+        print('beta ', beta)
+    beta = min(max(beta,minbeta),maxbeta)
     z = z - beta*z0
     x0 = xk
 if i==max_it-1:
@@ -221,9 +289,9 @@ r = A.dot(x0) - f
 for i in range(max_it):
     z = np.linalg.solve(R, np.linalg.solve(Q, r))
     alpha = np.dot(z.flatten(),A.dot(r).flatten())/np.dot(A.dot(z).flatten(),A.dot(z).flatten())
-    alpha = min(max(alpha,0.1),1.0)
     if log:
         print('alpha ', alpha)
+    alpha = min(max(alpha,minalpha),maxalpha)
     xk = x0 - alpha*z
     r = A.dot(xk) - f
     if log:
@@ -276,9 +344,9 @@ z = np.dot(Rm1h,np.linalg.solve(Q, r))
 z0 = z.copy()
 for i in range(max_it):
     alpha = np.dot(z.flatten(),r.flatten())/np.dot(z.flatten(),A.dot(z).flatten())
-    alpha = min(max(alpha,0.1),1.0)
     if log:
         print('alpha ', alpha)
+    alpha = min(max(alpha,minalpha),maxalpha)
     xk = x0 - alpha*z
     r = A.dot(xk) - f
     if log:
@@ -289,7 +357,9 @@ for i in range(max_it):
     z0 = z
     z = np.dot(Rm1h,np.linalg.solve(Q, r))
     beta = np.dot(z.flatten(),z0.flatten())/np.dot(z0.flatten(),z0.flatten())
-    # print('beta ', beta)
+    if log:
+        print('beta ', beta)
+    beta = min(max(beta,minbeta),maxbeta)
     z = z - beta*z0
     x0 = xk
 if i==max_it-1:
@@ -371,9 +441,9 @@ z = np.linalg.solve(Q, r)
 z0 = z.copy()
 for i in range(max_it):
     alpha = np.dot(z.flatten(),r.flatten())/np.dot(z.flatten(),A.dot(z).flatten())
-    alpha = min(max(alpha,0.1),1.0)
     if log:
         print('alpha ', alpha)
+    alpha = min(max(alpha,minalpha),maxalpha)
     xk = x0 - alpha*z
     r = A.dot(xk) - f
     if log:
@@ -384,7 +454,9 @@ for i in range(max_it):
     z0 = z
     z = np.linalg.solve(Q, r)
     beta = np.dot(z.flatten(),z0.flatten())/np.dot(z0.flatten(),z0.flatten())
-    # print('beta ', beta)
+    if log:
+        print('beta ', beta)
+    beta = min(max(beta,minbeta),maxbeta)
     z = z - beta*z0
     x0 = xk
 if i==max_it-1:
